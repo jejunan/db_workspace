96ed981949c3f2feab32a5bc2eb838fce9fb1207
jejunan <dkswpwns2@naver.com>
2025-06-14 01:58:27 +0900

    jwt based login dev

--- backend/src/main/java/com/onion/backend/controller/UserController.java
+++ backend/src/main/java/com/onion/backend/controller/UserController.java
@@ -1,13 +1,25 @@
 package com.onion.backend.controller;
 
+import com.onion.backend.dto.SignUpUser;
 import com.onion.backend.entity.User;                  // 사용자 엔티티 클래스 import
+import com.onion.backend.jwt.JwtUtil;
+import com.onion.backend.service.CustomUserDetailsService;
 import com.onion.backend.service.UserService;          // 사용자 서비스 클래스 import
 import io.swagger.v3.oas.annotations.Parameter;        // Swagger 문서화를 위한 어노테이션
 
 // REST API와 관련된 Spring 어노테이션
 import org.springframework.beans.factory.annotation.Autowired; // 의존성 주입을 위한 어노테이션
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;        // HTTP 응답을 감싸는 클래스
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.server.ResponseStatusException;
+
 
 import java.util.List;
 
@@ -15,10 +27,17 @@ import java.util.List;
 @RequestMapping("/api/users") // 헤당 컨트롤러의 기본 URL 경로는 '/api/users'
 public class UserController {
     private final UserService userService; // 사용자 관련 비즈니스 로직을 담당하는 클래스
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService userDetailsService;
 
     @Autowired // 생성자를 통해 userService가 의존성 주입
-    public UserController(UserService userService) {
+    public UserController(UserService userService, AuthenticationManager authenticationManager, JwtUtil jwtUtil, CustomUserDetailsService userDetailsService) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+
     }
 
     @GetMapping("") // HTTP GET 요청을 /api/users에 보내면 모든 사용자의 리스트를 반환
@@ -27,8 +46,8 @@ public class UserController {
     }
 
     @PostMapping("/signUp") // HTTP POST 요청을 /api/users/signUp에 보내면 회원가입이 이루어짐
-    public ResponseEntity<User> createUser(@RequestParam String username, @RequestParam String password, @RequestParam String email) {
-        User user = userService.createUser(username, password, email);
+    public ResponseEntity<User> createUser(@RequestBody SignUpUser signUpUser) {
+        User user = userService.createUser(signUpUser);
         return ResponseEntity.ok(user); // 회원가입된 사용자 객체를 응답
     }
 
@@ -39,5 +58,20 @@ public class UserController {
         userService.deleteUser(userId);
         // 삭제 후에는 204 No Content 상태 코드로 응답 (내용 없이 성공적으로 처리됨을 의미)
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password) throws AuthenticationException {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        return jwtUtil.generateToken(userDetails.getUsername());
+    }
+
+    @PostMapping("/token/validation")
+    @ResponseStatus(HttpStatus.OK)
+    public void jwtValidate(@RequestParam String token) {
+        if (!jwtUtil.validateToken(token)) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Token is not validation");
+        }
     }
 }
--- backend/src/main/java/com/onion/backend/service/UserService.java
+++ backend/src/main/java/com/onion/backend/service/UserService.java
@@ -1,8 +1,10 @@
 package com.onion.backend.service;
 
+import com.onion.backend.dto.SignUpUser;
 import com.onion.backend.entity.User; // 사용자 엔티티 클래스 import
 import com.onion.backend.repository.UserRepository; // 사용자 리포지토리 인터페이스 import
 import org.springframework.beans.factory.annotation.Autowired; // 의존성 주입을 위한 어노테이션
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service; // 서비스 컴포넌트임을 나타냄
 
 import java.time.LocalDateTime; // 날짜/시간 처리 클래스
@@ -12,18 +14,20 @@ import java.util.List; // 리스트 자료형 import
 public class UserService {
 
     private final UserRepository userRepository; // 사용자 DB 작업을 위한 Repository
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired // 생성자 기반 의존성 주입
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     // 사용자 생성 로직 (회원가입 등에서 사용)
-    public User createUser(String username, String password, String email) {
+    public User createUser(SignUpUser signUpUser) {
         User user = new User(); // 새 사용자 객체 생성
-        user.setUsername(username); // 사용자 이름 설정
-        user.setPassword(password); // 비밀번호 설정
-        user.setEmail(email);       // 이메일 설정
+        user.setUsername(signUpUser.getUsername()); // 사용자 이름 설정
+        user.setPassword(passwordEncoder.encode(signUpUser.getPassword())); // 비밀번호 설정
+        user.setEmail(signUpUser.getEmail());       // 이메일 설정
         return userRepository.save(user); // 사용자 정보를 DB에 저장 후 반환
     }
 
